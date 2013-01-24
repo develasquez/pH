43ebdbde1f0efadf4e49e332bfc11e4de0016cb9
felipe velasquez castro <felipe.velasquezc@gmail.com>
2013-01-24 16:03:49 -0300

    deleted

--- app/models/comment.rb
+++
@@ -1,3 +0,0 @@
-class Comment < ActiveRecord::Base
-  attr_accessible :delete, :message, :title, :user_id, :valoration
-end
--- app/models/user.rb
+++
@@ -1,3 +0,0 @@
-class User < ActiveRecord::Base
-  attr_accessible :city_id,  :email , :idDeleted , :name, :password, :photo_id, :rank, :status
-end
